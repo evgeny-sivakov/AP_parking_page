293b16f4c2e530d7e3eb5c80a60587da6375bf7b
Evgeny Sivakov <jsivakov@gmail.com>
2024-07-13 20:20:33 +0200

    add parking page

---
+++ src/app/fonts.ts
@@ -0,0 +1,4 @@
+import { Playfair_Display, Lato } from 'next/font/google';
+
+export const playfair_display = Playfair_Display({ subsets: ['cyrillic'] });
+export const lato = Lato({ subsets: ['latin'], weight: ['300'] });
